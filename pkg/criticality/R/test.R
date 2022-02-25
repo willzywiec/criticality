87f3c54eacb9b080708388b37e7c9aa8561cfcc6
Will Zywiec <30445407+wzywiec@users.noreply.github.com>
2022-02-25 15:48:20 -0800

    Update test.R

--- pkg/criticality/R/test.R
+++ pkg/criticality/R/test.R
@@ -53,7 +53,7 @@ Test <- function(
 
   nm.wt <- bfgs.wt <- sa.wt <- list()
 
-  Objective <- function(x) mean(abs(test.data$keff - rowSums(test.pred * x, na.rm = TRUE)))
+  Objective <- function(x) mean(abs(test.data$keff - rowSums(na.omit(test.pred) * x)))
 
   for (i in 1:meta.len) {
 
