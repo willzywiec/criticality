0174438e4f965abd49924bb3eaeb789f5be9a8f0
Will Zywiec <willzywiec@gmail.com>
2023-02-19 13:22:02 -0800

    Update tabulate.R

--- pkg/criticality/R/tabulate.R
+++ pkg/criticality/R/tabulate.R
@@ -42,97 +42,41 @@ Tabulate <- function(
   # load output
   #
     if (file.exists(paste0(ext.dir, '/', output.csv)) && !identical(output.csv, character(0))) {
+
       output <- utils::read.csv(paste0(ext.dir, '/', output.csv), fileEncoding = 'UTF-8-BOM') %>% stats::na.omit()
+
       if (nrow(output) >= length(output.files)) {
+
         output <- output[sample(nrow(output)), ]
+
+        # calculate vol (cc)
+        vol <- 4/3 * pi * output$rad^3
+
+        # calculate conc (g/cc)
+        conc <- output$mass / vol
+
+        output <- data.frame(
+          mass = output$mass,
+          form = output$form,
+          mod = output$mod,
+          rad = output$rad,
+          ref = output$ref,
+          thk = output$thk,
+          shape = output$shape,
+          vol = vol,
+          conc = conc,
+          keff = output$keff,
+          sd = output$sd)
+
         dataset <- Scale(code = code, output = output, ext.dir = ext.dir)
-      } else {
-        remove(output)
+
       }
+
+    } else {
+
+      stop('could not find data', call. = FALSE)
+
     }
-
-  #
-  # tabulate output
-  #
-    if (!exists('output') && length(output.files) == 0) stop('could not find data', call. = FALSE)
-
-    if (!exists('output')) {
-
-      mass <- rad <- thk <- ht <- vol <- conc <- hd <- keff <- sd <- numeric()
-      
-      form <- mod <- ref <- shape <- character()
-
-      for (i in 1:length(output.files)) {
-
-        if (any(grep('final result', readLines(paste0(ext.dir, '/', output.files[i]))))) {
-
-          # set mass (g), form, mod, rad (cm), and ref
-          file.name <- gsub('\\.o', '', output.files[i]) %>% strsplit('-') %>% unlist()
-          mass[i] <- as.numeric(file.name[1])
-          form[i] <- file.name[2]
-          mod[i] <- file.name[3]
-          rad[i] <- as.numeric(file.name[4])
-          ref[i] <- file.name[5]
-
-          # set thk (cm) and shape
-          if (ref[i] == 'none') {
-            thk[i] <- 0
-            shape[i] <- file.name[6]
-          } else {
-            thk[i] <- as.numeric(file.name[6])
-            shape[i] <- file.name[7]
-          }
-
-          # set ht (cm)
-          if (shape[i] == 'sph') {
-            ht[i] <- 2 * rad[i]
-          } else if (ref[i] == 'none') {
-            ht[i] <- as.numeric(file.name[7])
-          } else {
-            ht[i] <- as.numeric(file.name[8])
-          }
-
-          # calculate vol (cc)
-          if (shape[i] == 'sph') {
-            vol[i] <- 4/3 * pi * rad[i]^3
-          } else if (shape[i] == 'rcc') {
-            vol[i] <- pi * rad[i]^2 * ht[i]
-          }
-
-          # calculate conc (g/cc) and h/d
-          conc[i] <- (mass[i] / vol[i])
-          hd[i] <- (ht[i] / (2 * rad[i]))
-
-          # set keff and sd
-          final.result <- grep('final result', readLines(paste0(ext.dir, '/', output.files[i])), value = TRUE) %>% strsplit('\\s+') %>% unlist()
-          keff[i] <- final.result[4]
-          sd[i] <- final.result[5]
-
-        }
-
-      }
-
-      output <- data.frame(
-        mass = mass,
-        form = form,
-        mod = mod,
-        rad = rad,
-        ref = ref,
-        thk = thk,
-        shape = shape,
-        ht = ht,
-        vol = vol,
-        conc = conc,
-        hd = hd,
-        keff = keff,
-        sd = sd)
-
-      output <- output[sample(nrow(output)), ]
-      utils::write.csv(output, file = paste0(ext.dir, '/', code, '-output.csv'), row.names = FALSE)
-      
-      dataset <- Scale(code = code, output = output, ext.dir = ext.dir)
-
-    } 
 
   }
 
