cea63a85a962a88e878896209ccbcfcc13f6f380
Will Zywiec <willzywiec@gmail.com>
2023-09-03 10:58:28 -0700

    Update criticality

--- pkg/criticality/R/model.R
+++ pkg/criticality/R/model.R
@@ -46,6 +46,119 @@ Model <- function(
 
   model <- model %>% layer_dense(units = 1, activation = 'linear')
 
+#
+# refactor functions to select legacy optimizers for Apple M1/M2 chips
+# https://github.com/rstudio/keras/blob/main/R/utils.R
+#
+  if (Sys.info()[1] == 'Darwin') {
+
+    assert_all_dots_named <- function(envir = parent.frame(), cl) {
+
+      x <- eval(quote(list(...)), envir)
+
+      if(!length(x)) return()
+
+      x <- names(x)
+
+      if(is.character(x) && !anyNA(x) && all(x != "")) return()
+
+      stop('All arguments provided to `...` must be named.\n',
+        'Call with unnamed arguments in dots:\n  ',
+        paste(deparse(cl, 500L), collapse = '\n'))
+
+    }
+
+    capture_args <- function(
+      cl, modifiers = NULL, ignore = NULL,
+      envir = parent.frame(), fn = sys.function(-1)) {
+
+      fn_arg_nms <- names(formals(fn))
+      known_args <- intersect(names(cl), fn_arg_nms)
+      known_args <- setdiff(known_args, ignore)
+      names(known_args) <- known_args
+      cl2 <- c(quote(list), lapply(known_args, as.symbol))
+
+      if("..." %in% fn_arg_nms && !"..." %in% ignore) {
+        assert_all_dots_named(envir, cl)
+        cl2 <- c(cl2, quote(...))
+      }
+
+      args <- eval(as.call(cl2), envir)
+
+      # check `ignore` again, since arg might have been in `...`
+      for (nm in intersect(names(args), ignore)) {
+        args[[nm]] <- NULL
+      }
+
+      nms_to_modify <- intersect(names(args), names(modifiers))
+
+      for (nm in nms_to_modify) {
+        args[nm] <- list(modifiers[[nm]](args[[nm]]))
+      }
+
+    }
+
+    optimizer_adadelta <- function(
+      learning_rate = 0.001, rho = 0.95, epsilon = 1e-07,
+      weight_decay = NULL, clipnorm = NULL, clipvalue = NULL, global_clipnorm = NULL,
+      use_ema = FALSE, ema_momentum = 0.99, ema_overwrite_frequency = NULL,
+      jit_compile = TRUE, name = "Adadelta", ...) {
+      args <- capture_args(match.call(), NULL)
+      do.call(keras$optimizers$legacy$Adadelta, args)
+    }
+
+    optimizer_adagrad <- function(
+      learning_rate = 0.001, initial_accumulator_value = 0.1,
+      epsilon = 1e-07, weight_decay = NULL, clipnorm = NULL, clipvalue = NULL,
+      global_clipnorm = NULL, use_ema = FALSE, ema_momentum = 0.99,
+      ema_overwrite_frequency = NULL, jit_compile = TRUE,
+      name = 'Adagrad', ...) {
+      args <- capture_args(match.call(), NULL)
+      do.call(keras$optimizers$legacy$Adagrad, args)
+    }
+
+    optimizer_adam <- function(
+      learning_rate = 0.001, beta_1 = 0.9, beta_2 = 0.999,
+      epsilon = 1e-07, amsgrad = FALSE, weight_decay = NULL, clipnorm = NULL,
+      clipvalue = NULL, global_clipnorm = NULL, use_ema = FALSE,
+      ema_momentum = 0.99, ema_overwrite_frequency = NULL, jit_compile = TRUE,
+      name = 'Adam', ...) {
+      args <- capture_args(match.call(), NULL)
+      do.call(keras$optimizers$legacy$Adam, args)
+    }
+
+    optimizer_adamax <- function(
+      learning_rate = 0.001, beta_1 = 0.9, beta_2 = 0.999,
+      epsilon = 1e-07, weight_decay = NULL, clipnorm = NULL, clipvalue = NULL,
+      global_clipnorm = NULL, use_ema = FALSE, ema_momentum = 0.99,
+      ema_overwrite_frequency = NULL, jit_compile = TRUE,
+      name = 'Adamax', ...) {
+      args <- capture_args(match.call(), NULL)
+      do.call(keras$optimizers$legacy$Adamax, args)
+    }
+
+    optimizer_nadam <- function(
+      learning_rate = 0.001, beta_1 = 0.9, beta_2 = 0.999,
+      epsilon = 1e-07, weight_decay = NULL, clipnorm = NULL, clipvalue = NULL,
+      global_clipnorm = NULL, use_ema = FALSE, ema_momentum = 0.99,
+      ema_overwrite_frequency = NULL, jit_compile = TRUE,
+      name = 'Nadam', ...) {
+      args <- capture_args(match.call(), NULL)
+      do.call(keras$optimizers$legacy$Nadam, args)
+    }
+
+    optimizer_rmsprop <- function(
+      learning_rate = 0.001, rho = 0.9, momentum = 0, epsilon = 1e-07,
+      centered = FALSE, weight_decay = NULL, clipnorm = NULL, clipvalue = NULL,
+      global_clipnorm = NULL, use_ema = FALSE, ema_momentum = 0.99,
+      ema_overwrite_frequency = 100L, jit_compile = TRUE,
+      name = "RMSprop", ...) {
+      args <- capture_args(match.call(), list(ema_overwrite_frequency = as.integer))
+      do.call(keras$optimizers$legacy$RMSprop, args)
+    }
+
+  }
+
   if (opt.alg == 'adadelta') {
     model %>% compile(
       loss = loss,
