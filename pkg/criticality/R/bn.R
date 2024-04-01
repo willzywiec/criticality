27a4fbaf96b611d289391871ba0203fc06861d98
Will Zywiec <willzywiec@gmail.com>
2024-03-31 23:09:16 -0700

    Update bn.R

--- pkg/criticality/R/bn.R
+++ pkg/criticality/R/bn.R
@@ -11,10 +11,10 @@
 #' @export
 #' @examples
 #'
-#' ext.dir <- paste0(tempdir(), "/criticality/inst/extdata")
+#' ext.dir <- paste0(tempdir(), "/criticality/extdata")
 #' dir.create(ext.dir, recursive = TRUE, showWarnings = FALSE)
 #'
-#' extdata <- paste0(.libPaths()[1], "/criticality/inst/extdata")
+#' extdata <- paste0(.libPaths()[1], "/criticality/extdata")
 #' file.copy(paste0(extdata, "/facility.csv"), ext.dir, recursive = TRUE)
 #' file.copy(paste0(extdata, "/mcnp-dataset.RData"), ext.dir, recursive = TRUE)
 #'
